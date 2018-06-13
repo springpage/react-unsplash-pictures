cf44e4a8a8767bb234581ac924742f382a3d57d0
springpage <39781828+springpage@users.noreply.github.com>
2018-06-13 16:33:42 -0700

    Update index.js

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,5 +1,5 @@
 import axios from 'axios';
-const API_KEY ='6cd6366dbd3b88d40286217a220f5bd8df0d863c4f89903a08228e6608ad7dee';
+const API_KEY ='';
 const ROOT_URL=`https://api.unsplash.com/search/photos/?page=1&per_page=20&query=`;
 
 export const LOAD_IMAGE= 'LOAD_IMAGE';
